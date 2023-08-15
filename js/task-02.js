559cef49be9c8aa959a58ff5d1157f02df5cbce5
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-08-15 21:50:23 +0200

    task2-finish

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listsIngredients = document.querySelector("ul#ingredients");
+console.log(listsIngredients);
+
+ingredients.forEach((ingredient) => {
+  const itemList = document.createElement("li");
+  itemList.textContent = ingredient;
+  itemList.classList.add("item");
+  listsIngredients.prepend(itemList);
+});
