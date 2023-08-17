4669fc7b9a8e4f9512a0e47cb8304f5540e5b338
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-08-17 21:46:41 +0200

    task5-finish

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", updateValue);
+function updateValue(event) {
+  if ("input" === "") {
+    outputName.textContent = "Anonymous";
+  } else {
+    outputName.textContent = event.target.value;
+  }
+}
