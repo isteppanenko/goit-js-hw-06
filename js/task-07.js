3cb4259c2754fcba1ed6010ac80a42e0ab89805a
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-08-18 00:09:36 +0200

    task7-finish

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const inputControl = document.querySelector("#font-size-control");
+console.log(inputControl);
+const textSize = document.querySelector("#text");
+console.log(textSize);
+inputControl.addEventListener("input", updateSize);
+function updateSize(size) {
+  textSize.style.fontSize = size.target.value + "px";
+}
+textSize.style.fontSize = textSize;
