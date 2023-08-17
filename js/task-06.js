55da840dc0a549948da7353f756261fc7ce6b713
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-08-17 23:33:58 +0200

    task6-finish

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const validation = document.querySelector("#validation-input");
+const validationItem = parseInt(validation.dataset.length);
+
+validation.addEventListener("blur", (event) => {
+  if (validationItem === validation.value.length) {
+    validation.classList.remove("invalid");
+    validation.classList.add("valid");
+  } else {
+    validation.classList.remove("valid");
+    validation.classList.add("invalid");
+  }
+});
