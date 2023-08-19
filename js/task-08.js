0ddeb58b9ad1587c82a826a5a38e3c586a520de0
isteppanenko <128259876+isteppanenko@users.noreply.github.com>
2023-08-19 22:05:38 +0200

    task8-finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formObj = new FormData(event.currentTarget);
+  const formDataObj = {};
+
+  formObj.forEach((value, name) => {
+    formDataObj[name] = value;
+  });
+
+  if (formDataObj.email === "" || !formDataObj.password) {
+    alert("Всі поля повинні бути заповнені!");
+    return;
+  }
+
+  console.log(formDataObj);
+  form.reset();
+}
